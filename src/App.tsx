7ae4328c60022d04e8d1ef7beee07d0a23cbf10d
sonyhero <27anton06@gmail.com>
2023-03-27 23:04:36 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Frame} from './Components/Frame/Frame';
-import {Counter} from './Components/Counter/Counter';
+import {Display} from './Components/Display/Display';
+import {Value} from './Components/Value/Value';
+import SuperButton from './Components/Button/SuperButton';
+import s from './Components/Counter.module.css'
 
 function App() {
 
+    const maxValue = 5
+    const minValue = 0
 
-  return (
-    <div className={'App'}>
-        <Frame>
-        </Frame>
-        <Frame>
-            <Counter/>
-        </Frame>
-    </div>
-  );
+    const [number, setNumber] = useState<number>(minValue)
+
+    const onClickHandlerInc = () => setNumber(state => state+1)
+    const onClickHandlerReset = () => setNumber(0)
+
+    const finalClassName = (number === maxValue) ? s.error : s.normal
+
+    const incButton = number === maxValue
+
+    const resetButton = number === minValue
+
+
+    return (
+        <div className={'App'}>
+            <Frame>
+                <Display/>
+            </Frame>
+            <Frame>
+                <Display>
+                    <Value className={finalClassName}
+                           number={number}/>
+                </Display>
+                <div className={s.buttonWrapper}>
+                    <SuperButton className={s.inc}
+                                 disabled={incButton}
+                                 name={'inc'}
+                                 callBack={onClickHandlerInc}/>
+                    <SuperButton className={s.reset}
+                                 disabled={resetButton}
+                                 name={'reset'}
+                                 callBack={onClickHandlerReset}/>
+                </div>
+
+            </Frame>
+        </div>
+    );
 }
 
 export default App;
