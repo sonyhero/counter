2663983b08dba01b53d2b642f5d9f971ee3b7ee4
sonyhero <27anton06@gmail.com>
2023-04-01 20:59:59 +0300

    add-input

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,16 +1,21 @@
 import React, {ChangeEvent} from 'react';
+import s from './Input.module.css';
 
 type InputProps = {
-    title: string
-    setTitle: (title: string) => void
+    inputName: string
+    value: number
+    setValue: (newValue: number) => void
 }
 export const Input = (props: InputProps) => {
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        props.setTitle(e.currentTarget.value)
+        props.setValue(+e.currentTarget.value)
     }
 
     return (
-        <input value={props.title} onChange={onChangeHandler}/>
+        <div>
+            <div className={s.settingsSpan}>{props.inputName}: </div>
+            <input className={s.settingsInput} type={'number'} value={props.value} onChange={onChangeHandler}/>
+        </div>
     );
 };
