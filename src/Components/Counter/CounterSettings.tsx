112029420745d883cc7eeaa37e042295d60ec477
sonyhero <27anton06@gmail.com>
2023-04-01 20:59:42 +0300

    add-counter-settings

---
+++ src/Components/Counter/CounterSettings.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import s from './Counter.module.css';
+import {Input} from '../../Input/Input';
+import SuperButton from '../SuperButton/SuperButton';
+
+type CounterSettingsType = {
+    maxValue: number
+    minValue: number
+    setInputMaxValue: (newValue: number) => void
+    setInputMinValue: (newValue: number) => void
+    setCounter:()=>void
+}
+export const CounterSettings = (props: CounterSettingsType) => {
+    return (
+        <div className={s.wrap}>
+            <div className={s.display}>
+                <Input
+                    inputName={'max value'}
+                    value={props.maxValue}
+                    setValue={props.setInputMaxValue}/>
+                <Input
+                    inputName={'min value'}
+                    value={props.minValue}
+                    setValue={props.setInputMinValue}/>
+
+            </div>
+            <div className={s.buttonWrapper}>
+                <SuperButton
+                    name={'set'}
+                    callBack={props.setCounter}
+                    disabled={false}/>
+            </div>
+        </div>
+    )
+}
